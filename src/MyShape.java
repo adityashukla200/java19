547aaedc922f9f4bb4478babb11d9164ad01f197
adityashukla200 <shuklaaditya139@gmail.com>
2025-08-27 09:33:39 +0530

    java 27/08/2025

---
+++ src/MyShape.java
@@ -0,0 +1,9 @@
+public class MyShape {
+    public static void main(String[] args) {
+        NewShape s = new NewShape();
+        s.draw(12);
+//        s.draw(3,4);
+    }
+
+
+}
---
+++ src/calculator.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class calculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x,y,z=0;
+        System.out.println("Enter any two numbers");
+        x = sc.nextInt();
+        y = sc.nextInt();
+        try{
+            z=x/y;   //exception aa sakti hai 12/0=infinite
+        }
+
+        catch (ArithmeticException e){
+            System.out.println(e.getMessage());
+        }
+
+        catch (Exception e){     //brahmos
+            System.out.println(e.getMessage());
+        }
+
+        finally {
+            System.out.println(z);
+        }
+    }
+}
